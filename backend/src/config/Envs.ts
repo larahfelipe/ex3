9272ffe8d49932d4b15bd00cd2ff10fe4dd959e2
Felipe Lara <felipeahlara@gmail.com>
2023-07-07 14:42:01 -0300

    Chore: Dockerfile added

--- backend/src/config/Envs.ts
+++ backend/src/config/Envs.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 config();
 
 export const envs = {
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 8080,
   dbAccessUrl: process.env.DATABASE_URL,
   bcryptSalt: process.env.BCRYPT_SALT || 12,
   jwtSecret: process.env.JWT_SECRET || 'jwtSecret'
